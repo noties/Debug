88ef0e30a811405b4b0471afc444a7d5de3445cb
Dimitry Ivanov <mail@dimitryivanov.ru>
2016-03-10 23:47:03 +0100

    Added `apt-annotations` module

---
+++ apt-annotations/src/main/java/ru/noties/debug/apt/annotations/DebugConfiguration.java
@@ -0,0 +1,16 @@
+package ru.noties.debug.apt.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Dimitry Ivanov on 10.03.2016.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface DebugConfiguration {
+    boolean isDebug();
+    String label();
+}
