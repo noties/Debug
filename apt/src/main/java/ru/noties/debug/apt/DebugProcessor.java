740cfc5eecb99167bce149e1ec9583f8f8328900
Dimitry Ivanov <mail@dimitryivanov.ru>
2016-03-10 23:43:42 +0100

    Added `apt` module

---
+++ apt/src/main/java/ru/noties/debug/apt/DebugProcessor.java
@@ -0,0 +1,35 @@
+package ru.noties.debug.apt;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+
+/**
+ * Created by Dimitry Ivanov on 10.03.2016.
+ */
+public class DebugProcessor extends AbstractProcessor {
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return super.getSupportedAnnotationTypes();
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return super.getSupportedSourceVersion();
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
